fe19f74a308077e6529e098a9eb201df6bcf9155
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-10-30 18:48:02 +0300

    add

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,31 +1,31 @@
-// const whitelist = [
-//   'https://last.nomoredomains.work',
-//   'http://last.nomoredomains.work',
-//   'localhost:3000',
-// ];
+const whitelist = [
+  'https://last.nomoredomains.work',
+  'http://last.nomoredomains.work',
+  'localhost:3000',
+];
 
-// const corsOptionsDelegate = (req, callback) => {
-//   let corsOptions;
-//   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-//     corsOptions = { origin: true };
-//   } else {
-//     corsOptions = { origin: false };
-//   }
-//   callback(null, corsOptions);
-// };
-
-// module.exports = corsOptionsDelegate;
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  if (req.method === 'OPTIONS') {
-    res.send(200);
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
   }
-  else {
-    next();
-  }
+  callback(null, corsOptions);
 };
 
-module.exports = allowCrossDomain;
+module.exports = corsOptionsDelegate;
+// const allowCrossDomain = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//   if (req.method === 'OPTIONS') {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
+// };
+
+// module.exports = allowCrossDomain;
