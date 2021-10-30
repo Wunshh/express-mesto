1fd7b920c7bfaf2bdbc1bf8afeb0318827d85cbe
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-10-30 19:22:15 +0300

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,13 +1,36 @@
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://last.nomoredomains.work');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://last.nomoredomains.work',
+  'http://last.nomoredomains.work',
+  'localhost:3000',
+  'localhost:5000',
+];
 
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
+const allowCrossDomain = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
   }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
 };
 
 module.exports = allowCrossDomain;
+
+// const allowCrossDomain = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://last.nomoredomains.work');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+//   if (req.method === 'OPTIONS') {
+//     res.send(200);
+//   } else {
+//     next();
+//   }
+// };
