1e358472a197b95d5bb3efacf80cf77cb9715f06
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-10-31 21:52:33 +0300

    add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const {
   createUser,
@@ -10,14 +11,16 @@ const {
 } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
-const allowCrossDomain = require('./middlewares/cors');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(allowCrossDomain);
+app.use(cors({
+  credentials: true,
+  origin: ['https://last.nomoredomains.work', 'http://last.nomoredomains.work', 'localhost:3000'],
+}));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
