601ba396aab8cb9a967983144e285fa602c8f2e1
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-10-31 22:39:27 +0300

    add

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,21 +1,23 @@
-const allowedCors = [
-  'https://last.nomoredomains.work',
-  'http://last.nomoredomains.work',
-  'localhost:3000',
-  'localhost:5000',
-];
+// const allowedCors = [
+//   'https://last.nomoredomains.work',
+//   'http://last.nomoredomains.work',
+//   'localhost:3000',
+//   'localhost:5000',
+// ];
 
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', allowedCors);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.header('Access-Control-Allow-Credentials', 'true');
+// const allowCrossDomain = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
-};
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   next();
+// };
 
-module.exports = allowCrossDomain;
+// module.exports = allowCrossDomain;
