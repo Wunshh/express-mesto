81d52d1e587ddd31149a2e3f5773ecd31cc10d8e
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-11-01 12:45:01 +0300

    add

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const { errors } = require('celebrate');
-const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const {
   createUser,
@@ -10,13 +9,13 @@ const {
 } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
-// const allowCrossDomain = require('./middlewares/cors');
+const allowCrossDomain = require('./middlewares/cors');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(allowCrossDomain);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,23 +1,20 @@
-// const allowedCors = [
-//   'https://last.nomoredomains.work',
-//   'http://last.nomoredomains.work',
-//   'localhost:3000',
-//   'localhost:5000',
-// ];
+const allowedCors = [
+  'https://last.nomoredomains.work',
+  'http://last.nomoredomains.work',
+  'localhost:3000',
+  'localhost:5000',
+];
 
-// const allowCrossDomain = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowedCors);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', allowedCors);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   next();
-// };
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 
-// module.exports = allowCrossDomain;
+module.exports = allowCrossDomain;
