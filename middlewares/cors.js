d0b5b84aae5c0fa57c4322b5621faad90ebccced
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-11-03 20:49:47 +0300

    add

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,12 +1,21 @@
-const allowCrossDomain = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin');
+const allowedCors = [
+  'https://last.nomoredomains.work',
+  'http://last.nomoredomains.work',
+  'localhost:3000',
+  'localhost:5000',
+];
 
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    console.log({ message: 'что-то не так' });
+const allowCrossDomain = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
   }
   next();
 };
