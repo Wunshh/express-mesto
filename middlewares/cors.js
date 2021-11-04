a63d11dc3b4fa6e25a38a65d080503863dc89197
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-11-04 19:46:39 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -10,8 +10,8 @@ const allowCrossDomain = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+  if (allowedCors.includes(origin) ) {
+    res.header('Access-Control-Allow-Origin', '*');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
