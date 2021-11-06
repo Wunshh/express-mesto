2e00bb08382e799c7ca89fa077a67035e8eb69a3
Wunshh <81330170+Wunshh@users.noreply.github.com>
2021-11-06 13:37:36 +0300

    add

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ const {
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 
-const options = {
-  origin: ['*'],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-  credentials: true,
-};
+// const options = {
+//   origin: ['*'],
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+//   credentials: true,
+// };
 
 const app = express();
 const PORT = 3000;
 
-app.use('*', cors(options));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
